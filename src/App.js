a78c28aa66ec8d83ab9eb7fae995f5e227fbd26c
Mateus Borges <mateusgaspio@gmail.com>
2022-06-08 11:21:59 -0400

    Finalizado o projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css' //importa o CSS
-import { useState } from "react"; // importa o useState
- 
+import { useState } from "react"; // importa o useState 
 
 export default function App(){
-
   
-  const[] = useState() //Cria o useState
+  const[peso, setPeso] = useState('') //Cria o useState
+  const[altura, setAltura] = useState('')
+  const[msg, setMsg] = useState('')
+
+  function calculaIMC(){
+    // alert(`O peso é ${peso} e a altura é ${altura}`)
+    const p = peso
+    const a = altura / 100
+    const i = p / (a * a)
+    // Seu imc é de ${imc.toFixed(2)}
+    setMsg(`Seu IMC é de ${i.toFixed(2)}`)
+
+  }
 
   return(
     <div className="app">
       <h1>Calculadora IMC</h1>
       <span></span>
       <div className="ipts">
-        <input type='text' placeholder="Peso Kg Ex: 85.2"/>
-        <input type='text' placeholder="Altura cm Ex: 180"/>
-        <button>
+        <input type='text' placeholder="Peso Kg Ex: 85.2" value={peso} onChange={(e)=> setPeso(e.target.value)}/>
+        <input type='text' placeholder="Altura cm Ex: 180" value={altura} onChange={(e)=> setAltura(e.target.value)}/>
+        <button onClick={calculaIMC}>
           Calcular
         </button>
       </div>
-      <h2>Seu IMC foi tanto!!!!!!</h2>
+      <h2>{msg}</h2>
     </div>
   )
 }
